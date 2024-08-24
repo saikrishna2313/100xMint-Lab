030b37052e4512f17bb171f29b1648b24b501210
Sai Krishna Varma <142757076+saikrishna2313@users.noreply.github.com>
2024-08-23 23:09:01 -0700

    Update dropdown-menu.tsx

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -15,7 +15,7 @@ const DropdownMenuGroup = DropdownMenuPrimitive.Group
 const DropdownMenuPortal = DropdownMenuPrimitive.Portal
 
 const DropdownMenuSub = DropdownMenuPrimitive.Sub
-
+ 
 const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup
 
 const DropdownMenuSubTrigger = React.forwardRef<
